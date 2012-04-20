29fb50f82ccf152bd9ac698c35c70b9ebcfa0d38
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-04-20 13:40:07 -0500

    adding security, localizing config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -46,6 +46,11 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
+		compile ":spring-security-ldap:1.0.5.1"
+		compile ":spring-security-core:1.2.7.2"
+		compile ":spring-security-mock:1.0.1"
+
+
         build ":tomcat:$grailsVersion"
     }
 }
